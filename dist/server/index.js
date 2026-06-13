202c8b4a76c20848d399fe44f9a55ca8e5ff031e
mstivenvelezc-ctrl <mstivenvelezc@gmail.com>
2026-06-13 15:52:18 -0500

    primera coneccion

--- dist/server/index.js
+++ dist/server/index.js
@@ -1,4 +1,16 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-console.log(`MCP Server Ready !!`);
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+const server = new McpServer({
+    name: "mstivenvelezc-ctrl-agent",
+    version: "1.0.0",
+});
+async function main() {
+    const transport = new StdioServerTransport();
+    await server.connect(transport);
+    console.error("Server is running...");
+}
+main().catch((error) => {
+    console.error("Error starting the server:", error);
+    process.exit(1);
+});
 //# sourceMappingURL=index.js.map
